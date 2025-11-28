828d3e9c3e74e1bf2daa1cd620d68ff17127a428
Divyamdev1011 <119519112+Divyamdev1011@users.noreply.github.com>
2025-11-29 01:51:16 +0530

    Update schema.py

--- src/api/schema.py
+++ src/api/schema.py
@@ -1,12 +1,27 @@
 from pydantic import BaseModel
-from typing import List, Optional
+from typing import List
 
-class LineItem(BaseModel):
-    description: str
-    amount: Optional[float]
+class BillItem(BaseModel):
+    item_name: str
+    item_amount: float
+    item_rate: float
+    item_quantity: float
 
-class ExtractResponse(BaseModel):
-    line_items: List[LineItem]
-    calculated_total: float
-    original_text_snippet: Optional[str] = None
-    fraud: Optional[dict] = None
+class PageLineItems(BaseModel):
+    page_no: str
+    page_type: str
+    bill_items: List[BillItem]
+
+class TokenUsage(BaseModel):
+    total_tokens: int = 0
+    input_tokens: int = 0
+    output_tokens: int = 0
+
+class DataModel(BaseModel):
+    pagewise_line_items: List[PageLineItems]
+    total_item_count: int
+
+class ReportResponse(BaseModel):
+    is_success: bool
+    token_usage: TokenUsage
+    data: DataModel
